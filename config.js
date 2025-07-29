986db58dc9a5da47b8c65db60e284c7e1158ee36
Kavinduiduwara201 <gunathilakalayanal1968@gmail.com>
2025-07-29 22:24:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "tSF1VDpC#nlwoExK2lAGCOzVjJ66xLoDL84AlQPhXDed2DSlZA6I",
+  SESSION_ID: process.env.SESSION_ID || "ShadowXMD=lLNxHQqC#9kEOe93r8yh0Qgw7_XlD3nRiZpiVxNouXjJf9agXsXg",
   MONGODB: process.env.MONGODB || "mongodb://mongo:PuXQPOOttwxEypeYhNwzlnSMfJgVJSLM@switchyard.proxy.rlwy.net:11662",
-  OWNER_NUM: process.env.OWNER_NUM || "94764040298",
+  OWNER_NUM: process.env.OWNER_NUM || "94770051298",
 };
